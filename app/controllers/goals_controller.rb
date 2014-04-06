183d2a87149272c2244e24c91628d2f94b798911
cassiebrooks <cassie.brooks@af83.com>
2014-04-07 00:46:14 +0100

    add draper & goal decorator

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,13 +1,13 @@
 class GoalsController < ApplicationController
   def index
-    @goals = Goal.all(:order => "created_at DESC")
+    @goals = GoalDecorator.all(:order => "created_at DESC")
     respond_to do |format|
       format.html
     end
   end
 
   def create
-    @goal = Goal.create(:name => params[:name], :description => params[:description])
+    @goal = GoalDecorator.create(:name => params[:name], :description => params[:description])
     respond_to do |format|
       if @goal.save
         format.html { redirect_to goals_path }
