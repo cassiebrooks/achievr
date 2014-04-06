c59778ae957f689537673ca5a2288d62ab8d6c45
cassiebrooks <cassie.brooks@af83.com>
2014-04-06 12:42:21 +0100

    Create app & add Devise to user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Achievr::Application.config.secret_key_base = 'bbf9042249d9b8b63939839606774a21b0d32770e0e3e996ed99c99c25a4678ad25ed31514f40227488121882b489069b2b4d034f380857b07b912a9b7e19198'
