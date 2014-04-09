b3a77d00a5f6d90256c0f88b86930636ee92bb0e
Cassie Brooks <cassie.brooks@af83.com>
2014-04-09 18:37:21 +0200

    new task form working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,34 @@
 class TasksController < ApplicationController
+  before_filter :load_goal
+
   def index
-    goal_id = params[:goal_id]
-    @tasks = current_user.goals.find(goal_id).tasks
+    @tasks = @goal.tasks.all
+  end
+
+  def new
+    @task = @goal.tasks.new
+  end
+
+  def create
+    @task = @goal.tasks.new(task_params)
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to [@goal, @task], notice: "Task was successfully created."}
+      end
+    end
+  end
+
+  def show
+    @task = @goal.tasks.find(params[:id])
+  end
+
+  private
+
+  def load_goal
+    @goal = Goal.find(params[:goal_id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
