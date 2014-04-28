3539aacd4aac8bf1c3d3767a4e8d53f143411318
Cassie Brooks <cassie.brooks@af83.com>
2014-04-28 18:45:27 +0200

    remove "string" from Goal

---
+++ db/migrate/20140428164456_remove_string_from_goal.rb
@@ -0,0 +1,4 @@
+class RemoveStringFromGoal < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407185049) do
+ActiveRecord::Schema.define(version: 20140428164456) do
 
   create_table "goals", force: true do |t|
     t.string   "name"
